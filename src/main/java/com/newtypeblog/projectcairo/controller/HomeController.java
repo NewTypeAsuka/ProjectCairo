0323c873b7fc0b86440adcc0e800df74c9106b58
이상준 <sangzoon0102@naver.com>
2025-12-27 22:40:34 +0900

    약간 수정

--- src/main/java/com/newtypeblog/projectcairo/controller/HomeController.java
+++ src/main/java/com/newtypeblog/projectcairo/controller/HomeController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    /* 프로젝트 진입 지점 */
     @GetMapping("/")
     public String home() {
-        // templates/index.html 을 렌더링
-        return "index";
+        return "index"; // templates/index.html 을 렌더링
     }
 }
 
