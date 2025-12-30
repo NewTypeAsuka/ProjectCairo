0c1334999368c1254679ba53ba3167b1ee6d1f9c
JUNBOOK\LeeSangJun <sangzoon0102@naver.com>
2025-12-30 10:03:31 +0900

    주석 추가

--- src/main/java/com/newtypeblog/projectcairo/ProjectCairoApplication.java
+++ src/main/java/com/newtypeblog/projectcairo/ProjectCairoApplication.java
@@ -3,6 +3,22 @@ package com.newtypeblog.projectcairo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/* 부트스트랩 어노테이션: 시작을 시작하는 코드 */
+
+/*
+1. JVM에서 main() 실행
+2. SpringApplication 객체 생성
+3. SpringApplication.run()
+4. ApplicationContext 생성
+5. @SpringBootApplication 해석
+    ├─ @ComponentScan
+    ├─ @EnableAutoConfiguration
+6. 모든 빈 등록 및 의존성 주입
+7. 내장 Tomcat 생성
+8. 8080 포트 바인딩
+9. 서버 대기 상태 진입
+* */
+
 @SpringBootApplication
 public class ProjectCairoApplication {
 
--- src/main/java/com/newtypeblog/projectcairo/controller/HomeController.java
+++ src/main/java/com/newtypeblog/projectcairo/controller/HomeController.java
@@ -10,5 +10,6 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "index"; // templates/index.html 을 렌더링
+        // index.html은 관습적인 거고 home.html 등으로 바꿔도 상관은 없음
     }
 }
